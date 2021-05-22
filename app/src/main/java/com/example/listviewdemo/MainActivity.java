ca983dcb529d046cab57deb640abf481990fa6d0
Supriya-Shilwant <Supriya.shilwant06@gmail.com>
2021-05-22 22:07:06 +0530

    Added changes for list view project

---
+++ app/src/main/java/com/example/listviewdemo/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.listviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    String[] countries = {"China", "Australia", "Afghanistan", "Albania", "Mexico", "United Arab Emirates", "United States of America",
+            "India", "SriLanka", "Pakistan", "Bangladesh", "Zimbabwe"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_dropdown_item_1line, countries);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Click on : " + countries[position], Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
